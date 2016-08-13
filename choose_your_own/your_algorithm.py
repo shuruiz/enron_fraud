7f8576b4699caea6c20638086ceb75777458efa4
Ethan <zhangzhbd@gmail.com>
2016-08-13 23:53:28 +0800

    use KNN to classify the terrain data and choose K=3, the accuracy is 0.936 by KNN

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,15 +24,21 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
+#show origin data
+#plt.show() 
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
-
-
+#use KNN algorithm here
+from sklearn.neighbors import KNeighborsClassifier
+clf=KNeighborsClassifier(n_neighbors=3)
+clf=clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+print accuracy_score(pred,labels_test)
 
 
 
@@ -42,3 +48,4 @@ try:
     prettyPicture(clf, features_test, labels_test)
 except NameError:
     pass
+
