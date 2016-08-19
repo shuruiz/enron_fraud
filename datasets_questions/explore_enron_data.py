27c7d3a09ca688360a75ab048c45dd1bfef9f720
Ethan <zhangzhbd@gmail.com>
2016-08-19 14:20:43 +0800

    count how many people has totle_payment, count size of NaN in POI(0%), count how many people has email.

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -20,22 +20,52 @@ import pickle
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 #
 #count poi length or feature size
-# print len(enron_data)
+print " \n"
+print "E+F data set size:	"+str(len(enron_data))
 
-# print len(enron_data["SKILLING JEFFREY K"])
+print " \n"
+print "data set format {[LASTNAME FIRSTNAME MIDINIT],features},this is SKILLING JEFFREY K: \n"+str(enron_data["SKILLING JEFFREY K"])
 
-# countPOI=0
-# for person in enron_data:
-# 	if enron_data[person]["poi"]==1:
-# 		countPOI+=1
-# print countPOI
+countPOI=0
+for person in enron_data:
+	if enron_data[person]["poi"]==1:
+		countPOI+=1
+print " \n"
+print "POI size:	"+str(countPOI)
 
 #explore a person's features in enron_data(/data_question/explore_enron_data.py).
 
 # print enron_data["PRENTICE JAMES"]["total_stock_value"]
 # print enron_data["COLWELL WESLEY"]["from_this_person_to_poi"]
 
-print enron_data["SKILLING JEFFREY K"]["total_payments"]
-print enron_data["FASTOW ANDREW S"]["total_payments"]
-print enron_data["LAY KENNETH L"]["total_payments"]
+#compare money paid of CEO CFO and BOARD DIRECTOR
+
+# print enron_data["SKILLING JEFFREY K"]["total_payments"]
+# print enron_data["FASTOW ANDREW S"]["total_payments"]
+# print enron_data["LAY KENNETH L"]["total_payments"]
+
+has_salary_count=0
+has_email_count=0
+dont_has_totle_payment=0
+poi_size=0
+poi_not_has_totle_payment_size=0
+for folk in enron_data:
+	if enron_data[folk]["salary"] !='NaN':
+		has_salary_count+=1
+	if enron_data[folk]["email_address"] !='NaN':
+		has_email_count+=1
+	if enron_data[folk]["total_payments"]=='NaN':
+		dont_has_totle_payment+=1
+	if enron_data[folk]["poi"] and enron_data[folk]["total_payments"]=='NaN':
+		poi_not_has_totle_payment_size+=1
+print " \n"
+#How many POIs in the E+F dataset have NaN for their total payments
+print "has_qualified_salary_count:	  "+str(has_salary_count)
+print "has_email_count: 	 "+str(has_email_count)
+print " \n"
+print "dont_has_totle_payment_count:	"+str(dont_has_totle_payment)
+print "dont_has_totle_payment fraction:	"+str(dont_has_totle_payment*1.0/(len(enron_data)))
+print " \n"
+print "poi_not_has_totle_payment_percentage:	"+str(poi_not_has_totle_payment_size*1.0/countPOI)
+
 
