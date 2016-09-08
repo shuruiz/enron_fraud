32b2478565a47f7fd4029d0d39f7f5deb9799618
Ethan <zhangzhbd@gmail.com>
2016-09-08 17:36:04 -0400

    remove some outliers but not sure remove the right one, LAY KENNETH L and SKILLING JEFFREY K remians

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -9,11 +9,25 @@ from feature_format import featureFormat, targetFeatureSplit
 
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
+data_dict.pop("TOTAL",0)
+#data_dict.pop("LAY KENNETH L",0)
+#data_dict.pop('SKILLING JEFFREY K',0)
+data_dict.pop('ALLEN PHILLIP K',0)
+data_dict.pop('BELDEN TIMOTHY N',0)
+data_dict.pop('LAVORATO JOHN J',0)
+data_dict.pop('FREVERT MARK A',0)
 features = ["salary", "bonus"]
 data = featureFormat(data_dict, features)
 
 
 ### your code below
 
+for point in data:
+    salary = point[0]
+    bonus = point[1]
+    matplotlib.pyplot.scatter( salary, bonus )
 
+matplotlib.pyplot.xlabel("salary")
+matplotlib.pyplot.ylabel("bonus")
+matplotlib.pyplot.show()
 
